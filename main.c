4e0324f8d99204f7af88a5e175b62a8aab46f4d7
Pedro <pguerrape@gmail.com>
2026-09-09 19:33:39 -0300

    validacao de argumentos da linha de comando

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "scheduler.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s <rate|edf> <arquivo_de_entrada>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "rate") != 0 && strcmp(argv[1], "edf") != 0) {
+        fprintf(stderr, "Erro: Algoritmo invalido '%s'. Escolha 'rate' ou 'edf'.\n", argv[1]);
+        return 1;
+    }
+
     return 0;
 }
--- scheduler.h
+++ scheduler.h
@@ -6,4 +6,46 @@
 #include <string.h>
 #include <stdbool.h>
 
-#endif // SCHEDULER_H
+#define MAX_NAME_LEN 32
+#define MAX_TASKS 64
+
+typedef struct {
+    char name[MAX_NAME_LEN];
+    int period;
+    int deadline;
+    int burst;
+    int file_order;
+} Task;
+
+typedef struct {
+    int total_time;
+    int num_tasks;
+    Task tasks[MAX_TASKS];
+} SimulationData;
+
+#endif
+#define SCHEDULER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_NAME_LEN 32
+#define MAX_TASKS 64
+
+typedef struct {
+    char name[MAX_NAME_LEN];
+    int period;
+    int deadline;
+    int burst;
+    int file_order;
+} Task;
+
+typedef struct {
+    int total_time;
+    int num_tasks;
+    Task tasks[MAX_TASKS];
+} SimulationData;
+
+#endif
